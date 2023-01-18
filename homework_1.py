df05c3e95959fabb9c0a81664b9dd278e1fe4542
Hex Howk <hexhowk@ya.ru>
2023-01-19 01:23:03 +0300

    2nd task added

--- homework_1.py
+++ homework_1.py
@@ -13,3 +13,17 @@ assert domain_name("http://google.com") == "google"
 assert domain_name("http://google.co.jp") == "google"
 assert domain_name("www.xakep.ru") == "xakep"
 assert domain_name("https://youtube.com") == "youtube"
+
+
+
+# Написать метод int32_to_ip, который принимает на вход 32-битное целое число (integer)
+# и возвращает строковое представление его в виде IPv4-адреса:
+
+def int32_to_ip(int32):
+    return '.'.join([str(int32 >> (i << 3) & 0xFF)
+        for i in range(4)[::-1]])
+
+
+assert int32_to_ip(2154959208) == "128.114.17.104"
+assert int32_to_ip(0) == "0.0.0.0"
+assert int32_to_ip(2149583361) == "128.32.10.1"
---
+++ readme.md
@@ -0,0 +1,17 @@
+#Tasks from Doka for Homework
+
+##Задача №1.
+Написать метод domain_name, который вернет домен из url адреса:
+
+```url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+url = "https://www.cnet.com"                -> domain name = "cnet"```
+
+
+##Задача №2.
+Написать метод int32_to_ip, который принимает на вход 32-битное целое число (integer) и возвращает строковое представление его в виде IPv4-адреса:
+
+```2149583361 -> "128.32.10.1"
+32         -> "0.0.0.32"
+0          -> "0.0.0.0"```
+
