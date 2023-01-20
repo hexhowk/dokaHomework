583ff96cc2d8766908560d12787384d9f7e3be4b
Hex Howk <hexhowk@ya.ru>
2023-01-20 16:22:16 +0300

    3, 4 tasks added

--- homework_1.py
+++ homework_1.py
@@ -5,7 +5,6 @@ def domain_name(url):
     start = url[start_index:]
     ending = start.find(".")
     domain = start[:ending]
-    print(domain)
     return domain
 
 
@@ -27,3 +26,56 @@ def int32_to_ip(int32):
 assert int32_to_ip(2154959208) == "128.114.17.104"
 assert int32_to_ip(0) == "0.0.0.0"
 assert int32_to_ip(2149583361) == "128.32.10.1"
+
+
+
+# Написать метод zeros, который принимает на вход целое число (integer)
+# и возвращает количество конечных нулей в факториале (N! = 1 * 2 * 3 * ... * N) заданного числа:
+
+def zeros(n):
+    res = 0 
+    while n > 0:
+        n = n // 5
+        res += n
+    return res
+
+
+assert zeros(0) == 0
+assert zeros(6) == 1
+assert zeros(30) == 7
+
+
+
+# Написать метод bananas, который принимает на вход строку
+# и возвращает количество слов «banana» в строке.
+
+from functools import lru_cache
+
+@lru_cache(None)
+def items(word, schema):
+    res = []
+    if schema == "":
+        res.append("".rjust(len(word), '-'))
+        return res
+    for i in range(len(word)):
+        if schema[0] == word[i]:
+            left_s = "".rjust(i, '-') + word[i]
+            if word[i + 1 :] == "" and schema[1:] == "":
+                res.append(left_s)
+            else:
+                right_s_list = items(word[i + 1 :], schema[1:])
+                for j in right_s_list:
+                    res.append(left_s + j)
+    return res
+
+def bananas(word) -> set:
+    return set(items(word, "banana"))
+
+
+assert bananas("banann") == set()
+assert bananas("banana") == {"banana"}
+assert bananas("bbananana") == {"b-an--ana", "-banana--", "-b--anana", "b-a--nana", "-banan--a",
+                     "b-ana--na", "b---anana", "-bana--na", "-ba--nana", "b-anan--a",
+                     "-ban--ana", "b-anana--"}
+assert bananas("bananaaa") == {"banan-a-", "banana--", "banan--a"}
+assert bananas("bananana") == {"ban--ana", "ba--nana", "bana--na", "b--anana", "banana--", "banan--a"}
